d13c66219f8f82ea47fd81d3c69702f47e616eb0
harshilarora2005 <harshil.arora205@gmail.com>
2026-03-23 01:22:03 +0530

    switched to postgres

--- scripts/db_init.sql
+++
@@ -1,8 +0,0 @@
--- Drop database if it exists
-DROP DATABASE IF EXISTS DataWarehouse;
-
--- Create database
-CREATE DATABASE DataWarehouse;
-
--- Select database
-USE DataWarehouse;
--- scripts/proc_load_bronze.sql
+++ scripts/proc_load_bronze.sql
@@ -1,53 +1,88 @@
-DELIMITER $$
-
-CREATE PROCEDURE load_bronze()
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
 BEGIN
 
-    -- CRM TABLES
-    TRUNCATE TABLE bronze_crm_cust_info;
+    RAISE NOTICE '========================================';
+    RAISE NOTICE 'Loading Bronze Layer Started';
+    RAISE NOTICE '========================================';
 
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/cust_info.csv'
-    INTO TABLE bronze_crm_cust_info
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
+    -- CRM CUSTOMER INFO
+    RAISE NOTICE 'Loading crm_cust_info...';
+    TRUNCATE TABLE bronze.crm_cust_info;
 
-    TRUNCATE TABLE bronze_crm_prd_info;
+    COPY bronze.crm_cust_info
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/cust_info.csv'
+    DELIMITER ','
+    CSV HEADER;
 
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/prd_info.csv'
-    INTO TABLE bronze_crm_prd_info
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
-
-    TRUNCATE TABLE bronze_crm_sales_details;
-
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/sales_details.csv'
-    INTO TABLE bronze_crm_sales_details
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
+    RAISE NOTICE 'crm_cust_info loaded successfully';
 
 
-    -- ERP TABLES
-    TRUNCATE TABLE bronze_erp_loc_a101;
+    -- CRM PRODUCT INFO
+    RAISE NOTICE 'Loading crm_prd_info...';
+    TRUNCATE TABLE bronze.crm_prd_info;
 
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/LOC_A101.csv'
-    INTO TABLE bronze_erp_loc_a101
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
+    COPY bronze.crm_prd_info
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/prd_info.csv'
+    DELIMITER ','
+    CSV HEADER;
 
-    TRUNCATE TABLE bronze_erp_cust_az12;
+    RAISE NOTICE 'crm_prd_info loaded successfully';
 
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/CUST_AZ12.csv'
-    INTO TABLE bronze_erp_cust_az12
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
 
-    TRUNCATE TABLE bronze_erp_px_cat_g1v2;
+    -- CRM SALES DETAILS
+    RAISE NOTICE 'Loading crm_sales_details...';
+    TRUNCATE TABLE bronze.crm_sales_details;
 
-    LOAD DATA INFILE '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/PX_CAT_G1V2.csv'
-    INTO TABLE bronze_erp_px_cat_g1v2
-    FIELDS TERMINATED BY ','
-    IGNORE 1 ROWS;
+    COPY bronze.crm_sales_details
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_crm/sales_details.csv'
+    DELIMITER ','
+    CSV HEADER;
 
-END$$
+    RAISE NOTICE 'crm_sales_details loaded successfully';
 
-DELIMITER ;
+
+    -- ERP LOCATION
+    RAISE NOTICE 'Loading erp_loc_a101...';
+    TRUNCATE TABLE bronze.erp_loc_a101;
+
+    COPY bronze.erp_loc_a101
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/LOC_A101.csv'
+    DELIMITER ','
+    CSV HEADER;
+
+    RAISE NOTICE 'erp_loc_a101 loaded successfully';
+
+
+    -- ERP CUSTOMER
+    RAISE NOTICE 'Loading erp_cust_az12...';
+    TRUNCATE TABLE bronze.erp_cust_az12;
+
+    COPY bronze.erp_cust_az12
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/CUST_AZ12.csv'
+    DELIMITER ','
+    CSV HEADER;
+
+    RAISE NOTICE 'erp_cust_az12 loaded successfully';
+
+
+    -- ERP PRODUCT CATEGORY
+    RAISE NOTICE 'Loading erp_px_cat_g1v2...';
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+    COPY bronze.erp_px_cat_g1v2
+    FROM '/Users/harshilarora/Documents/date_warehouse_project/datasets/source_erp/PX_CAT_G1V2.csv'
+    DELIMITER ','
+    CSV HEADER;
+
+    RAISE NOTICE 'erp_px_cat_g1v2 loaded successfully';
+
+    RAISE NOTICE '========================================';
+    RAISE NOTICE 'Bronze Layer Loading Completed';
+    RAISE NOTICE '========================================';
+
+END;
+$$;
+
+CALL bronze.load_bronze();
